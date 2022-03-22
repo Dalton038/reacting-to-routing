569b6a29c552c1930469799e5f68e552e9697e59
Dalton Hall <dhall.07113@gmail.com>
2022-03-22 14:39:52 -0500

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import FilmList from "./components/FilmList";
 import PeopleList from "./components/PeopleList";
 
